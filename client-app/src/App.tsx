d9e973e55d7db94a85b40d1b778957d79db99a39
Majhi, Prmaod <pramod.majhi@blueshieldca.com>
2020-03-25 13:55:33 -0700

    added docker-compose-pilot.yaml and change the app name integrate GA code

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import { Provider } from 'react-redux'
@@ -6,8 +6,21 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import store from './store';
 import { Routes } from './routes'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import { createBrowserHistory } from 'history'
+import ReactGA from 'react-ga'
+
+
+ReactGA.initialize('UA-138450916-4');
+const browserHistory = createBrowserHistory()
+browserHistory.listen((location, action) => {
+  ReactGA.pageview(location.pathname + location.search)
+})
 
 const App: React.FC = () => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search)
+  }, [])
+
   return (
     <MuiThemeProvider>
       <Provider store={store}>
